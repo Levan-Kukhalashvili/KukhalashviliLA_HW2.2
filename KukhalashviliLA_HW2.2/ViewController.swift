8f581954a5fb0794d7115dc7234b33489dbaa9cd
KukhalashviliLA <48877065+KukhalashviliLA@users.noreply.github.com>
2020-10-31 13:20:07 +0300

    1.0.0.1

--- KukhalashviliLA_HW2.2/ViewController.swift
+++ KukhalashviliLA_HW2.2/ViewController.swift
@@ -7,6 +7,10 @@
 
 import UIKit
 
+enum CurrentLight {
+    case red, green, blue
+}
+
 class ViewController: UIViewController {
 
     @IBOutlet var colorView: UIView!
@@ -23,31 +27,48 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         setupСustomization()
-        colorSliderAction()
+        setColor()
+        setValue(for: currentRedValue, currentGreenValue, currentBlueValue)
     }
 
-    @IBAction func colorSliderAction() {
-        let redValue = CGFloat(redSlider.value)
-        let greenValue = CGFloat(greenSlider.value)
-        let blueValue = CGFloat(blueSlider.value)
-        
-        currentRedValue.text = String(format: "%.2f", redSlider.value)
-        currentGreenValue.text = String(format: "%.2f", greenSlider.value)
-        currentBlueValue.text = String(format: "%.2f", blueSlider.value)
-        
-        colorView.backgroundColor = UIColor(
-            red: redValue,
-            green: greenValue,
-            blue: blueValue,
-            alpha: 1)
+    @IBAction func colorSliderAction(_ sender: UISlider) {
+        setColor()
+        switch sender.tag {
+        case 0: currentRedValue.text = formatValue(slider: sender)
+        case 1: currentGreenValue.text = formatValue(slider: sender)
+        case 2: currentBlueValue.text = formatValue(slider: sender)
+        default: break
+        }
     }
     
     private func setupСustomization() {
-        colorView.layer.cornerRadius = 30
+        colorView.layer.cornerRadius = 20
         
         redSlider.tintColor = .red
         greenSlider.tintColor = .green
         blueSlider.tintColor = .blue
+    }
+    
+    private func setColor() {
+        colorView.backgroundColor = UIColor(red: CGFloat(redSlider.value),
+                                            green: CGFloat(greenSlider.value),
+                                            blue: CGFloat(blueSlider.value),
+                                            alpha: 1)
+    }
+    
+    private func setValue(for labels: UILabel...) {
+        labels.forEach { label in
+            switch label.tag {
+            case 0: currentRedValue.text = formatValue(slider: redSlider)
+            case 1: currentGreenValue.text = formatValue(slider: greenSlider)
+            case 2: currentBlueValue.text = formatValue(slider: blueSlider)
+            default: break
+            }
+        }
+    }
+    
+    private func formatValue(slider: UISlider) -> String {
+        String(format: "%.2f", slider.value)
     }
 }
 
